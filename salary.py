b379e0a5e4151df6c6168d3e535a9323c02ee41c
AnnapoornaSB02 <anubkatti02@gmail.com>
2025-11-10 16:17:22 +0530

    Update salary.py

--- salary.py
+++ salary.py
@@ -1,16 +1,10 @@
 import sys
-
 if len(sys.argv) == 2:
-    script_name = sys.argv[0]
     salary = float(sys.argv[1])
-    print("User provided salary.")
 else:
-    script_name = sys.argv[0]
-    salary = 50000
-    print("No input given – using default salary")
+    print("No input provided. Using default salary: 50000")
+    salary = 50000.0
 bonus = salary * 0.10
 total_salary = salary + bonus
-print("\nScript Name:", script_name)
-print("Basic Salary : ", salary)
-print("Bonus (10%)  : ", bonus)
-print("Total Salary : ", total_salary)
+print("Bonus Amount:", bonus)
+print("Total Salary after adding Bonus:", total_salary)
